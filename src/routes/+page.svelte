bc3e799118096e35435fe4f742ef9b50069541b9
Adam Rackis <adamrackis@hotmail.com>
2024-07-11 18:32:40 -0500

    Work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Svelte 5 Demo</h1>
+<div class="p-4">
+	<div class="flex flex-col gap-3">
+		<h1 class="text-lg">Svelte 5 Demo</h1>
 
-<a href="/svelte5">Svelte 5 Features in isolation</a>
+		<div class="flex flex-col gap-3 ml-3">
+			<a href="/svelte5">Svelte 5 Features in isolation</a>
+		</div>
+	</div>
+</div>
