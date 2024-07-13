4791c897ceb2d802d0cce0d8913be752551a9bdd
Adam Rackis <adamrackis@hotmail.com>
2024-07-12 21:08:32 -0500

    work

--- src/routes/svelte5/+page.svelte
+++ src/routes/svelte5/+page.svelte
@@ -2,9 +2,16 @@
 	import NumberInfo from './NumberInfo.svelte';
 	import RenderChildComponent from './RenderChildComponent.svelte';
 	import State from './State.svelte';
+	import AutoComplete from './AutoComplete.svelte';
+
+	let items = [{ name: 'Adam' }, { name: 'Rich' }];
+	let onSelect = (item: { name: string }) => {
+		console.log(item.name);
+	};
 </script>
 
 <div class="flex flex-col gap-2 p-4">
+	<AutoComplete {items} {onSelect} />
 	<RenderChildComponent />
 
 	<State />
---
+++ src/routes/svelte5/AutoComplete.svelte
@@ -0,0 +1,14 @@
+<script lang="ts" generics="T extends { name: string }">
+	type Props<T> = {
+		items: T[];
+		onSelect: (item: T) => void;
+	};
+
+	let { items, onSelect }: Props<T> = $props();
+</script>
+
+{#each items as item}
+	<div>
+		<button onclick={() => onSelect(item)}>Select</button>
+	</div>
+{/each}
